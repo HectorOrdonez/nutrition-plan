6230296c1cb196d72dd7cc149c43f2519e43087c
HectorOrdonez <hjavierog@gmail.com>
2016-05-24 23:15:25 +0100

    Added helpers to composer

--- config/app.php
+++ config/app.php
@@ -155,7 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Laralib\L5scaffold\GeneratorsServiceProvider::class
     ],
 
     /*
