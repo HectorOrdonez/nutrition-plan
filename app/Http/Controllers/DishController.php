3735862b639d977b72269ffd945368c424135904
Hector Ordonez <hjavierog@gmail.com>
2016-05-27 12:40:14 +0200

    Dish destruction

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -6,6 +6,10 @@ use App\Dish;
 
 class DishController extends Controller
 {
+    const MESSAGE_CREATED = 'Dish created successfully';
+    const MESSAGE_UPDATED = 'Dish updated successfully';
+    const MESSAGE_DELETED = 'Dish deleted successfully';
+
     /**
      * @todo Filter dishes by own and shared
      */
@@ -24,5 +28,22 @@ class DishController extends Controller
         $dish = Dish::find($id);
 
         return view('dishes.show', compact('dish'));
+    }
+
+    /**
+     * Destroys the dish and its ingredients
+     *
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $dish = Dish::find($id);
+        $dish->delete();
+
+        return redirect()
+            ->route('dishes.index')
+            ->with('flash_message', self::MESSAGE_DELETED);
+
     }
 }
