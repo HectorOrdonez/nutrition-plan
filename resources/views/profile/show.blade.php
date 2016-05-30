bd6cff6037a5f1447385054f048ac9d7668c085c
Hector Ordonez <hjavierog@gmail.com>
2016-05-30 15:29:47 +0200

    Profile fixes. Now User can update its nutritional targets

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,10 +5,11 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Profile</div>
+                    <div class="panel-heading"><h2>Profile</h2></div>
 
                     <div class="panel-body">
-                        {!! Form::open(array('route' => route('profile.update')) !!}
+                        <div class="well well-sm">Your nutritional targets</div>
+                        {!! Form::model($user, array('route' => 'profile.update', 'method' => 'put')) !!}
 
                         <div class="form-group @if($errors->has('calories')) has-error @endif">
                             {!! Form::label('calories-field', 'Calories') !!}
@@ -18,28 +19,28 @@
                             @endif
                         </div>
                         <div class="form-group @if($errors->has('proteins')) has-error @endif">
-                            {!! Form::label('proteins-field', 'Calories') !!}
+                            {!! Form::label('proteins-field', 'Proteins') !!}
                             {!! Form::text('proteins', old('proteins'), ['class' => 'form-control', 'id' => 'proteins->field'])!!}
                             @if($errors->has("proteins"))
                                 <span class="help-block">{{ $errors->first("proteins") }}</span>
                             @endif
                         </div>
                         <div class="form-group @if($errors->has('fats')) has-error @endif">
-                            {!! Form::label('fats-field', 'Calories') !!}
+                            {!! Form::label('fats-field', 'Fats') !!}
                             {!! Form::text('fats', old('fats'), ['class' => 'form-control', 'id' => 'fats->field'])!!}
                             @if($errors->has("fats"))
                                 <span class="help-block">{{ $errors->first("fats") }}</span>
                             @endif
                         </div>
                         <div class="form-group @if($errors->has('carbohydrates')) has-error @endif">
-                            {!! Form::label('carbohydrates-field', 'Calories') !!}
+                            {!! Form::label('carbohydrates-field', 'Carbohydrates') !!}
                             {!! Form::text('carbohydrates', old('carbohydrates'), ['class' => 'form-control', 'id' => 'carbohydrates->field'])!!}
-                            <label for="carbohydrates-field">Carbohydrates</label>
-                            <input type="text" id="carbohydrates-field" name="carbohydrates" class="form-control" value="{{ old("carbohydrates") }}"/>
                             @if($errors->has("carbohydrates"))
                                 <span class="help-block">{{ $errors->first("carbohydrates") }}</span>
                             @endif
                         </div>
+
+                        <button type="submit" class="btn btn-primary">Update</button>
 
                         {!! Form::close() !!}
                     </div>
